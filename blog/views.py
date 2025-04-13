ab7ad8d06e0cc8abd2a534e639337224de239dc1
Jacky Fogel <jackyfogel@gmail.com>
2025-04-13 19:52:12 +0300

    added blog fields

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 # Create your views here.
 
 def blog_home(request):
     return render(request,'blog/index.html')
+
+def post_detail(request, slug):
+    post = get_object_or_404(BlogPost, slug=slug)
+    return render(request, 'blog/post_detail.html', {'post': post})
