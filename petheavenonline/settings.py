a8f88b2b93af0660d5a0467d7002d19ee263f17b
jackyfogel <jackyfogel@gmail.com>
2025-04-17 11:32:33 +0300

    postgres sync

--- petheavenonline/settings.py
+++ petheavenonline/settings.py
@@ -82,18 +82,15 @@ WSGI_APPLICATION = 'petheavenonline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# Default: SQLite for local development
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=not DEBUG  # 💯 SSL only if not in debug mode
+    )
 }
-
-# If DATABASE_URL is set (like on Render), use PostgreSQL instead
-if os.environ.get('DATABASE_URL'):
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
